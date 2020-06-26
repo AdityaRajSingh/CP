95b0ea92146e846ba7da16ecfe08a4abad156a8f
AdityaRajSingh <adi4adityaraj@gmail.com>
2020-06-26 09:57:55 +0530

    26 June 2020

---
+++ Codeforces/01 Game.cpp
@@ -0,0 +1,57 @@
+														//ADITYA RAJ SINGH//
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+typedef std::vector<ll> vll;
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define endl '\n'
+#define pb push_back
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rep1(i,a,b) for(ll i=a;i<=b;i++)
+//ll M=1e+9;
+ll power(ll x, ll y, ll M)  
+{   ll res = 1; x = x % M; if (x == 0) return 0;  while (y > 0)  {    if (y & 1)  res = (res*x) % M;    y = y>>1; x = (x*x) % M;  }  return res;  } 
+ll modInverse(ll a, ll M) 
+{   ll g = __gcd(a, M); if (g != 1) return -1; else return(power(a, M-2, M)); } 
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("/home/aadi/Documents/input.txt", "r", stdin);
+	    freopen("/home/aadi/Documents/output.txt", "w", stdout);
+	#endif
+	fast;
+	
+	ll t; 
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		int l=s.length();
+		int c0=0,c1=0;
+		rep(i,0,l)
+		{
+			if(s[i]=='0')
+				c0++;
+			else
+				c1++;
+		}
+		int m=min(c0,c1);
+		if(m%2==1)
+		{
+			cout<<"DA"<<endl;
+		}
+		else
+		{
+			cout<<"NET"<<endl;
+		}
+
+
+
+
+
+	}	
+	return 0;
+}
