70b6afa4d0dc818307f48be40c9274bb1c56452e
AdityaRajSingh <adi4adityaraj@gmail.com>
2020-07-31 00:57:44 +0530

    30July2020

---
+++ Codeforces/Captain Flint and a Long Voyage.cpp
@@ -0,0 +1,78 @@
+														//ADITYA RAJ SINGH//
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+typedef std::vector<ll> vll;
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define endl '\n'
+#define pb push_back
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rep1(i,a,b) for(ll i=a;i<=b;i++)
+//ll M=1e+9;
+ll power(ll x, ll y, ll M)  
+{   ll res = 1; x = x % M; if (x == 0) return 0;  while (y > 0)  {    if (y & 1)  res = (res*x) % M;    y = y>>1; x = (x*x) % M;  }  return res;  } 
+ll modInverse(ll a, ll M) 
+{   ll g = __gcd(a, M); if (g != 1) return -1; else return(power(a, M-2, M)); } 
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("/home/aadi/Documents/input.txt", "r", stdin);
+	    freopen("/home/aadi/Documents/output.txt", "w", stdout);
+	#endif
+	fast;
+	
+
+
+	ll t,n; 
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n==1)
+			cout<<8<<endl;
+		else if(n==2)
+			cout<<98<<endl;
+		else if(n==3)
+			cout<<998<<endl;
+		else
+		{
+			if(n%4==0)
+			{
+				ll q=n/4;
+				rep(i,0,n-q)
+				{
+					cout<<9;
+				}
+				rep(i,0,q)
+				{
+					cout<<8;
+				}
+				cout<<endl;
+			}
+			else
+			{
+				ll q=n/4;
+				rep(i,0,n-q-1)
+				{
+					cout<<9;
+				}
+				cout<<8;
+				rep(i,0,q)
+				{
+					cout<<8;
+				}
+				cout<<endl;
+			}
+		}
+
+
+		
+
+
+
+
+	}	
+	return 0;
+}
