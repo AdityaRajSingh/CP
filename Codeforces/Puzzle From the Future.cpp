9b4e734866b119b0ec3f812af7da763e6c01dfe5
AdityaRajSingh <adi4adityaraj@gmail.com>
2021-01-27 23:00:13 +0530

    Code @ Wednesday 27 January 2021 11:00:08 PM IST

---
+++ Codeforces/Puzzle From the Future.cpp
@@ -0,0 +1,71 @@
+														//ADITYA RAJ SINGH//
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+typedef std::vector<ll> vll;
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define endl '\n'
+#define pb push_back
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rep1(i,a,b) for(ll i=a;i<=b;i++)
+//ll M=1e+9;
+ll power(ll x, ll y, ll M)  
+{   ll res = 1; x = x % M; if (x == 0) return 0;  while (y > 0)  {    if (y & 1)  res = (res*x) % M;    y = y>>1; x = (x*x) % M;  }  return res;  } 
+ll modInverse(ll a, ll M) 
+{   ll g = __gcd(a, M); if (g != 1) return -1; else return(power(a, M-2, M)); } 
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("/home/aadi/Documents/input.txt", "r", stdin);
+	    freopen("/home/aadi/Documents/output.txt", "w", stdout);
+	#endif
+	fast;
+	
+	ll t,n; 
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		ll a[n];
+
+		string s;
+		cin>>s;
+
+
+
+		rep(i,0,n)
+		{
+			a[i]=s[i]-'0';
+			// cout<<a[i]<<" ";
+		}
+		// cout<<endl;
+
+		if(n==1)
+			cout<<1<<endl;
+		else
+		{
+			ll b[n];
+			rep(i,0,n)
+				b[i]=1;
+			rep(i,1,n)
+			{
+				
+				if((a[i-1]+b[i-1])==(a[i]+b[i]))
+				{
+					b[i]=0;
+				}
+			}
+			rep(i,0,n)
+				cout<<b[i];
+			cout<<endl;
+		}
+
+
+
+
+
+	}	
+	return 0;
+}
